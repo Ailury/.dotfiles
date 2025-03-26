ff3950ff8f750231ec9b7534d7fc5df9c84cd783
Ailury <aileris@proton.me>
2025-03-27 00:09:42 +0100

    Initial commit

--- scripts/install-zshplugins.sh
+++ scripts/install-zshplugins.sh
@@ -5,7 +5,8 @@ check_and_add_string() {
     
     # Check if the source exists in the zshrc
     if ! grep -qF -- "$string" "$file"; then
-        printf "%s\n" "$string" >> "$file"
+	echo "source $HOME/.zsh/$string" >> "$file"
+        #printf "%s\n" "$string" >> "$file"
         echo "String added to file."
     else
         echo "Source already exists in file"
@@ -17,13 +18,13 @@ zsh_plugin_dir=$HOME/.zsh
 mkdir $zsh_plugin_dir
 # Install theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $zsh_plugin_dir/powerlevel10k
-plugin='source ${zsh_plugin_dir}/powerlevel10k/powerlevel10k.zsh-theme'
+plugin='powerlevel10k/powerlevel10k.zsh-theme'
 check_and_add_string $plugin
 
 git clone https://github.com/zsh-users/zsh-autosuggestions $zsh_plugin_dir/zsh-autosuggestions
-plugin='source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh'
+plugin='zsh-autosuggestions/zsh-autosuggestions.zsh'
 check_and_add_string $plugin
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_plugin_dir/zsh-syntax-highlighting
-plugin='source ${zsh_plugin_dir}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+plugin='zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
 check_and_add_string $plugin
